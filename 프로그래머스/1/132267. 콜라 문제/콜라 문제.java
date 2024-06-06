c5cfbb3a4ad3e1e6be9503072eea41e89a074fe2
Ssong94 <112993223+ssong94@users.noreply.github.com>
2024-06-06 22:26:22 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,14 +2,13 @@ class Solution {
     public int solution(int a, int b, int n) {
 		int answer = 0;
 
-		final int requiredColaBottleCount = a;
-		final int exchangedColaCount = b;
-		int ownedColaBottleCount = n;
+		while (n >= a) {
+			int changedCola = n / a;
+			int remainColaBottle = (n % a);
 
-		while (ownedColaBottleCount >= requiredColaBottleCount) {
-			answer += exchangedColaCount;
-			ownedColaBottleCount += exchangedColaCount;
-			ownedColaBottleCount -= requiredColaBottleCount;
+			answer += changedCola * b;
+
+			n = remainColaBottle + (changedCola * b);
 		}
 
 		return answer;
